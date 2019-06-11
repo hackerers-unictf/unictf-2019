ef7590eb84663043be9d4bec364f3036a611aae6
Valentjno <valentinomerlino@gmail.com>
2019-06-11 15:56:27 +0200

    without csrf token

--- services/EXIF_CTF/resources/views/index.blade.php
+++ services/EXIF_CTF/resources/views/index.blade.php
@@ -22,7 +22,7 @@
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
           <form enctype="multipart/form-data" method="POST" action="{{ route('home.show') }}">
-          @csrf
+
             <div class="form-row">
               <div class="col-12 col-md-9 mb-2 mb-md-0">
                 <input type="file" accept="image/jpeg" class="form-control form-control-lg" name="file">
